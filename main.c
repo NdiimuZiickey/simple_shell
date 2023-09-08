a14053fcdaec1bbdaf0cf9b7ac2cb06ef58b66d0
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-09-07 23:49:06 -0700

    dividing command line into several parts

--- main.c
+++ main.c
@@ -5,26 +5,51 @@
  * @argv: arguments passed
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	char *shell = "shell $ ";
-	char *lineptr = NULL;
+	char *lineptr = NULL, *lineptr_cp = NULL;
 	size_t n = 0;
-	ssize_t eof;
-	(void)argc, (void)argv;
+	ssize_t nread;
+	const char *dm = " \n";
+	char *word, **words;
+	int nword, i;
 
-	while (1)
-	{
-		printf("%s", shell);
-		eof = getline(&lineptr, &n, stdin);
-		if (eof == -1)
+
+		printf("shell $ ");
+		nread = getline(&lineptr, &n, stdin);
+		lineptr_cp = malloc(sizeof(char) * nread);
+		if (lineptr_cp == NULL)
+		{
+			perror("tsh: memoy not allocated");
+			return (-1);
+		}
+		strcpy(lineptr_cp, lineptr);
+		if (nread == -1)
 		{
 			printf("exit...\n");
 			return (-1);
 		}
-		printf("%s", lineptr);
-		free(lineptr);
-		lineptr = NULL;
-	}
-	return (0);
+		else
+		{
+			word = strtok(lineptr, dm);
+			for (nword = 1; word != NULL; nword++)
+				word = strtok(NULL, dm);
+			words = malloc(sizeof(char *) * (nword + 1));
+			word = strtok(lineptr_cp, dm);
+			for (i = 0; word != NULL; i++)
+			{
+				words[i] = malloc(sizeof(char) * strlen(word));
+				strcpy(words[i], word);
+				printf(">>>%s\n", words[i]);
+				word = strtok(NULL, dm);
+			}
+			words[i] = NULL;
+
+
+			free(words);
+			free(lineptr);
+			free(lineptr_cp);
+		}
+
+		return (0);
 }
