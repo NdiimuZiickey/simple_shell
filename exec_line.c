1a2396cb5fb8edf8f7b24ef14ee0e05ea0a47e96
NdiimuZiickey <zeeisa518@gmail.com>
2023-09-17 02:43:07 -0700

    Edits

--- exec_line.c
+++ exec_line.c
@@ -1,16 +1,22 @@
 #include "main.h"
-/**
- * exec - execute commands
- * @words: commands array
- */
-void exec(char **words)
-{
-	char *command = NULL;
 
-	if (words != NULL)
-	{
-		command = words[0];
-		if (execve(command, words, NULL) == -1)
-			perror("Error:");
-	}
+/**
+ * exec_line - finds builtins and commands
+ *
+ * @datash: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
 }
--- main.c
+++ main.c
@@ -1,56 +1,73 @@
 #include "main.h"
+
 /**
- * main - printing prompt
- * @argc: no of arguments passed
- * @argv: arguments passed
- * Return: 0
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-int main(void)
+void free_data(data_shell *datash)
 {
-	char *lineptr = NULL, *lineptr_cp = NULL;
-	size_t n = 0;
-	ssize_t nread;
-	const char *dm = " \n";
-	char *word, **words;
-	int nword, i;
+	unsigned int i;
 
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
 
-		printf("shell $ ");
-		nread = getline(&lineptr, &n, stdin);
-		lineptr_cp = malloc(sizeof(char) * nread);
-		if (lineptr_cp == NULL)
-		{
-			perror("tsh: memoy not allocated");
-			return (-1);
-		}
-		strcpy(lineptr_cp, lineptr);
-		if (nread == -1)
-		{
-			printf("exit...\n");
-			return (-1);
-		}
-		else
-		{
-			word = strtok(lineptr, dm);
-			for (nword = 1; word != NULL; nword++)
-				word = strtok(NULL, dm);
-			words = malloc(sizeof(char *) * (nword + 1));
-			word = strtok(lineptr_cp, dm);
-			for (i = 0; word != NULL; i++)
-			{
-				words[i] = malloc(sizeof(char) * strlen(word));
-				strcpy(words[i], word);
-				word = strtok(NULL, dm);
-			}
-			words[i] = NULL;
+	free(datash->_environ);
+	free(datash->pid);
+}
 
-			exec(words);
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
 
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
 
-			free(words);
-			free(lineptr);
-			free(lineptr_cp);
-		}
+	for (i = 0; environ[i]; i++)
+		;
 
-		return (0);
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
